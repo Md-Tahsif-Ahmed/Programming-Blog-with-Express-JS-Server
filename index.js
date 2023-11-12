f29508da51fe0fce8a84c7eb9b83f8dfe525a099
tahsif <tahsif.cse@gmail.com>
2023-11-12 22:07:34 +0600

    DB connection setup and run server

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtgohj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log("Connected to MongoDB!");
+    const blogsCollection = client.db("BlogDB").collection("blogs");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+run();
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-console.log(`app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
