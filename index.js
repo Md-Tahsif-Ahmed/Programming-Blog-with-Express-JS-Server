b45561801294ff89654f6ab3a87ad56a1a21e613
tahsif <tahsif.cse@gmail.com>
2023-11-14 14:08:03 +0600

    Created add wishlist

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+const BlogDB = "ProgrammingBlog";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtgohj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient
@@ -21,7 +21,34 @@ async function run() {
     // Connect the client to the server
     await client.connect();
     console.log("Connected to MongoDB!");
-    const blogsCollection = client.db("BlogDB").collection("blogs");
+    const blogsCollection = client.db(BlogDB).collection("blogs");
+    const wishlistCollection = client.db(BlogDB).collection("wishlist");
+
+    // Routes
+    app.get('/allblogs', async (req, res) => { // Added forward slash here
+      const cursor = blogsCollection.find();
+      const result = await cursor.toArray();
+    //   console.log(result);
+      res.send(result);
+    });
+
+    // server
+    app.get('/wishlist/:email', async (req, res)=>{
+      const email = req.params.email;
+      console.log(email);
+      const cursor = wishlistCollection.find({email:email});
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
+    app.post('/wishlist/:email', async (req, res)=>{
+      const newWishlist = req.body;
+      const result = await wishlistCollection.insertOne(newWishlist);
+      console.log(result)
+      res.send(result);
+    })
+
+  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
