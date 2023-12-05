12b521df8ee46d767f873100332e45b8f71a71bd
tahsif <tahsif.cse@gmail.com>
2023-12-05 09:45:34 +0600

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -15,11 +16,32 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 const BlogDB = "ProgrammingBlog";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtgohj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient
 const client = new MongoClient(uri);
+// middlewares
+const logger = (req, res, next)=>{
+   console.log('log: info', req.method, req.url);
+   next();
+}
+const verifyToken = (req, res, next)=>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // next();
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 async function run() {
   try {
@@ -43,9 +65,15 @@ app.post('/jwt', async (req, res)=>{
   .send({success: true});
 })
 
+app.post('/logout', async (req, res)=>{
+  const user = req.body;
+  console.log('logging out', user);
+  res.clearCookie('token', { maxAge: 0 }).send({ success: true }); 
 
+})
  // Routes
-app.get('/allblogs', async (req, res) => {
+app.get('/allblogs', logger, async (req, res) => {
+  
   const options = {
     // Sort returned documents in descending order by date (latest first)
     sort: { date: -1 },
@@ -54,6 +82,11 @@ app.get('/allblogs', async (req, res) => {
   };
   const cursor = blogsCollection.find({}, options); // Pass the options object as the second parameter
   const result = await cursor.toArray();
+  console.log('token owner info', req.user);
+  
+  // if(req.user.email !== req.query.email){
+  //   return res.status(403).send({message: 'forbidden access'})
+  // }
   //   console.log(result);
   res.send(result);
 });
